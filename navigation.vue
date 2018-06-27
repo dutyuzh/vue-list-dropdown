aeb7c3555c0db4a5db2182f7c8cc974dc4265d45
dutyuzh <dutyuzh@gmail.com>
2018-06-27 13:20:24 -0400

    main navigation tempalte
    this is a main parent template that will hold our component

---
+++ navigation.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+        <router-link to="/" class="navbar-brand">Home</router-link>
+        <dropdown id="component-dropdown" :options="userOptions"></dropdown>
+    </nav>
+</template>
+
+<script>
+    import Dropdown from '@/components/dropdown'
+
+    export default {
+        components: {
+            'dropdown': Dropdown
+        },
+
+        data () {
+            return {
+                selectedUser: 'Apple',
+                userOptions: {
+                    'Apple': 'Apple',
+                    'Banana': 'Banana',
+                    'Blueberry': 'Blueberry',
+                    'Kiwi': 'Kiwi',
+                    'Pear': 'Pear',
+                    'Pineapple': 'Pineapple',
+                    'Watermelon': 'Watermelon'
+                }
+            }
+        }
+    }
+</script>
