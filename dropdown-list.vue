3174921b42a1fc56a1f64d7b651603314b6a4055
dutyuzh <dutyuzh@gmail.com>
2018-06-27 13:21:59 -0400

    dropdown-list
    this is a component for rendering a dropdown list

---
+++ dropdown-list.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <li class="nav-item dropdown" :class="{ 'show': showDropDown}">
+            <a href="#" v-on:click.prevent="showDropDown=!showDropDown" class="nav-link dropdown-toggle">First Last</a>
+            <div v-if="showDropDown" class="dropdown-menu" :class="{ 'show': showDropDown}">
+                <a class="dropdown-item" href="#" v-for="(option, name) in options" :value="option">{{ name }}</a>
+            </div>
+        </li>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        data() {
+            return {
+                showDropDown: true,
+            }
+        },
+        props: {
+            options: {
+                type: Object,
+                required: true
+            }
+        },
+
+    }
+</script>
